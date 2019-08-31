8b1213bdcbd1f5ab3ce71ac26cb87ad68117f98a
EriksonBahr <erikson.bahr@gmail.com>
2019-08-31 15:58:14 -0300

    Created talks controller

---
+++ src/Controllers/TalksController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [ApiController]
+    [Route("api/camps/{moniker}/{controller}")]
+    public class TalksController: ControllerBase
+    {
+        private readonly ICampRepository campRepository;
+        private readonly IMapper mapper;
+        private readonly LinkGenerator linkGenerator;
+
+        public TalksController(ICampRepository campRepository, IMapper mapper, LinkGenerator linkGenerator)
+        {
+            this.campRepository = campRepository;
+            this.mapper = mapper;
+            this.linkGenerator = linkGenerator;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TalkModel[]>> GetTalks(string moniker)
+        {
+            try
+            {
+                var talks = await campRepository.GetTalksByMonikerAsync(moniker);
+                if (talks == null)
+                    return NotFound();
+
+                return mapper.Map<TalkModel[]>(talks);
+            }
+            catch (Exception e)
+            {
+
+                return StatusCode(StatusCodes.Status500InternalServerError, "GetTalks - " + e.Message);
+            }
+
+            return BadRequest();
+        }
+        
+    }
+}
